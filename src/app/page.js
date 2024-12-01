d471bbb127a2b91dad38952e589bc24b38a9b7b3
Thomas Hallström <minimoto@hotmail.com>
2024-12-01 13:15:31 +0100

    Last update in main

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,15 @@
 export default function Home() {
   return (
-    <div>
-      <h1>detta är ett test att se om det kan funka</h1>
-      <h2>Och det verkar det göra</h2>
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-xl font-semibold">Saboms schedule planing</h1>
+        <h2>Detta kommer förhoppningsvis bli en start på</h2>
+        <h2>hur ni kan jobba med scheman in framtiden</h2>
+        <h3>
+          Men det jobbas bara på denna när det finns tid så det är en lång
+          tidsplan
+        </h3>
+      </div>
     </div>
   );
 }
